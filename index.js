34b4d34353e7344e356528b243fd0c283a4b7fc4
skshig <skshig@gmail.com>
2023-03-04 16:35:33 +0530

    new post modal

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
+//this function opens a modal upon clicking Create New Psot button
 function openForm(){
     let openform = document.getElementById('modal-container').style.display = "flex";
-    console.log("hello")
+    // console.log("hello");
 }
+//this function closes the modal upon clicking x button
 function closeForm (){
-    let closeform = document.getElementById('close').style.display = "none";
+    let closeform = document.getElementById('modal-container').style.display = "none";
+}
+function publishBlog(){
+    // let publishBlog = document.getElementById('blog-post').style.display = "block";
+    let blog_title = document.getElementById('nnew-blog-heading').value;
+    let blog_description = document.getElementById('new-blog-description').value;
+    console.log(blog_title);
+    console.log(blog_description);
 }
