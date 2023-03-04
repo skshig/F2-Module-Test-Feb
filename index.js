432cde705c583ac9ff6c622c97c33e54cf0e5789
skshig <skshig@gmail.com>
2023-03-04 14:04:30 +0530

    ui covered

---
+++ index.js
@@ -0,0 +1,6 @@
+function openForm(){
+    let openform = document.getElementById('header-button"').style.display = "block";
+}
+function closeForm{
+    let closeform = document.getElementById('close').style.display = "none";
+}
