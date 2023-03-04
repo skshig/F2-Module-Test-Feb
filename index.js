184836f09d75540b8dd2c71a651025b178473f2c
skshig <skshig@gmail.com>
2023-03-04 17:08:44 +0530

    new post modal

--- index.js
+++ index.js
@@ -8,9 +8,22 @@ function closeForm (){
     let closeform = document.getElementById('modal-container').style.display = "none";
 }
 function publishBlog(){
-    // let publishBlog = document.getElementById('blog-post').style.display = "block";
-    let blog_title = document.getElementById('nnew-blog-heading').value;
+    let blog_title = document.getElementById('new-blog-heading').value;
     let blog_description = document.getElementById('new-blog-description').value;
     console.log(blog_title);
     console.log(blog_description);
+    let blog_list = document.getElementById('list-item');
+    let new_blog = document.createElement('li');
+    new_blog.innerHTML = `<div class="blog-post" id="blog-post"><h4>${blog_title}</h4><br>
+    <p>${blog_description}</p>
+    <span>
+        <label for="edit-button"></label>
+        <input type="button" name="edit-button" id="edit-button" value="Edit Post">
+    <span>
+        <label for="delete-button"></label>
+        <input type="button" name="delete-button" id="delete-button" value="Delete Post"></span>
+    </span>
+    <span><h5>Created at: </h5></span></div>`
+    blog_list.appendChild(new_blog);
+    closeForm();
 }
